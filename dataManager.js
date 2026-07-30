5a0d08a3ea00415c5cca56682016595f7693c08b
ILN44DIEZ <t5rsf28g2s@privaterelay.appleid.com>
2026-07-30 19:57:13 +0200

    Update dataManager.js

--- dataManager.js
+++ dataManager.js
@@ -1,1 +1,105 @@
-skskk
+class DataManager {
+
+    constructor() {
+
+        this.clubs = [];
+
+        this.loaded = false;
+
+    }
+
+
+
+    async loadClubs() {
+
+        try {
+
+            const response =
+                await fetch("data/clubs.json");
+
+
+            this.clubs =
+                await response.json();
+
+
+            this.loaded = true;
+
+
+            console.log(
+                "✅ Clubs chargés :",
+                this.clubs.length
+            );
+
+
+        } catch(error) {
+
+
+            console.error(
+                "❌ Erreur chargement clubs",
+                error
+            );
+
+
+        }
+
+    }
+
+
+
+    getLeagues() {
+
+        let leagues =
+            [];
+
+
+        this.clubs.forEach(club => {
+
+
+            if (!leagues.includes(club.ligue)) {
+
+                leagues.push(
+                    club.ligue
+                );
+
+            }
+
+
+        });
+
+
+        return leagues;
+
+    }
+
+
+
+    getClubsByLeague(league) {
+
+
+        return this.clubs.filter(
+
+            club =>
+                club.ligue === league
+
+        );
+
+
+    }
+
+
+
+    getClub(name) {
+
+
+        return this.clubs.find(
+
+            club =>
+                club.nom === name
+
+        );
+
+
+    }
+
+
+}
